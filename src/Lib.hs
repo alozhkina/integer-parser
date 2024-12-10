e5f60ebad01032db150b86a1cd95166d4f0778a6
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-10 19:22:39 +0300

    change all. from lessons

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,23 +2,30 @@ module Lib
     ( 
     ) where
 
+import Data.Text (Text)
+import qualified Data.Text as T
 import Control.Applicative ( (<$>), some)
-import Text.Parsec
-import Text.Parsec.String (Parser)
+import Data.Char (isDigit, digitToInt)
 
+newtype Parser a = Parser { runParser :: Text -> Maybe (Text, a) }
 
--- Парсер для числа
-number :: Parser Int
-number = read <$> some digit
+instance Functor Parser where
+    -- хотим применить func над результатом парсера p
+    fmap func (Parser p) = Parser f where
+        -- парсер f возвращает:
+        f input = case p input of
+            Nothing -> Nothing -- Nothing, если парсер p возвращает Nothing
+            Just (cs, c) -> Just (cs, func c) -- (остаток, c обработанный функцией func), если p возвращает (остаток, resP)
 
--- Парсер для бинарной операции
-binOperation :: Parser (Int -> Int -> Int)
-binOperation = do
-    op <- oneOf "+-*/"
-    return $ case op of
-        '+' -> (+)
-        '-' -> (-)
-        '*' -> (*)
-        '/' -> div
-        _   -> error "Unknown operator"
+-- Парсит символ, если он соответствует предикату
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy predicate = Parser f where
+        f input = case T.uncons input of
+                Just (c, rest) | predicate c -> Just (rest, c)
+                _ -> Nothing
+
+-- Парсит цифру и преобразует её в Int
+digit :: Parser Int
+digit = digitToInt <$> satisfy isDigit
+
 
